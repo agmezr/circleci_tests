082bf53c1b11f7f8ab041811163ce4c9609d89eb
Alex <alexgomez2202@gmail.com>
2020-11-06 21:58:47 -0600

    Test simple config circle ci

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,9 +1,20 @@
 import unittest
+import time
 
 class ATests(unittest.TestCase):
 
-    def tests_a(self):
+    def test_a(self):
+        time.sleep(3)
         self.assertTrue(True)
+
+    def test_b(self):
+        time.sleep(10)
+        self.assertTrue(True)
+
+    def test_c(self):
+        time.sleep(30)
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
