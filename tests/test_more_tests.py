172bd0e4e92c92f89a39e03f556013bae9023446
Alex <alexgomez2202@gmail.com>
2020-11-06 22:58:02 -0600

    Added more and more tests

---
+++ tests/test_more_tests.py
@@ -0,0 +1,45 @@
+import unittest
+import time
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        time.sleep(15)
+        self.assertTrue(True)
+
+    def test_some_1(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_2(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_3(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_4(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_5(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_6(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_7(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+    def test_some_8(self):
+        time.sleep(5)
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
