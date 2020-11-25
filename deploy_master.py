905a2b99c17bf89cfd5de69b9ac1879a5380b888
Alejandro Gomez <alex@umba.com>
2020-11-25 11:08:11 -0600

    Added deploy script and stuff to make it work

---
+++ deploy_master.py
@@ -0,0 +1,78 @@
+"""Deploys the develop branch to master"""
+import argparse
+import subprocess
+import sys
+
+VERSION_FILE = "app/version.py"
+
+parser = argparse.ArgumentParser(description="Deploy env to the prod server")
+parser.add_argument(
+    "version",
+    nargs=1,
+    help="Version release for master, must be greater that current version",
+)
+parser.add_argument(
+    "--tests",
+    "-t",
+    dest="validate",
+    action="store_true",
+    help="Run all the tests before creating the PR.",
+)
+parser.add_argument(
+    "--no-tests",
+    "-n",
+    dest="validate",
+    action="store_false",
+    help="Skip the tests before the PR.",
+)
+parser.set_defaults(validate=True)
+args = parser.parse_args()
+
+
+def main():
+    """Main function to make the steps needed to deploy to prod."""
+    version = args.version[0]
+    branch_name = f"release/{version}"
+    print("Creating release for version")
+
+    if args.validate:
+        print("Checking lint")
+        result = subprocess.run(["bash", "lint_check.sh"])
+        if result.returncode != 0:
+            print("Lint check failed, please verify it and try again.")
+            sys.exit(1)
+
+        print("Running tests")
+        result = subprocess.run(["make", "tests_parallel"])
+        if result.returncode != 0:
+            print("Some tests failed, please verify it and try again.")
+            sys.exit(1)
+
+
+    # create release branch
+    subprocess.run(["git", "flow", "release", "start", version])
+
+    # bump version
+    print("Bump version on version file")
+    with open(VERSION_FILE, "w+") as f:
+        f.write(f'APP_VERSION = "{version}"')
+
+    print("Creating and pushing release")
+    # commit new version
+    subprocess.run(["git", "commit", "-a", version, "-m", version])
+
+    # push new version
+    subprocess.run(["git", "push", "origin", branch_name])
+
+    for env in ("develop", "master"):
+        print("Merge to ", env)
+        subprocess.run(["git", "checkout", env])
+        subprocess.run(["git", "merge", branch_name])
+        subprocess.run(["git", "push", "origin", env, "--follow-tags"])
+
+    print("Deploy done!")
+
+
+if __name__ == "__main__":
+    main()
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("This is just a test")
+print("of something new")
 
 
